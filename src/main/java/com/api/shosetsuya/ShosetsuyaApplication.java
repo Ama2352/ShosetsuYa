a78136dadd359b2cfa6bead93c568c321a1f1db5
Hồ Nguyễn Minh Sang <165858646+Ama2352@users.noreply.github.com>
2025-08-23 21:59:54 +0700

    first commit

---
+++ src/main/java/com/api/shosetsuya/ShosetsuyaApplication.java
@@ -0,0 +1,13 @@
+package com.api.shosetsuya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShosetsuyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShosetsuyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/shosetsuya/ShosetsuyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.shosetsuya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShosetsuyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
